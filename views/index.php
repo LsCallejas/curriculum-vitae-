b01770c1bf717ceaa928b097a493299b30028ca9
Juan Carlos <LsCallejas>
2023-04-07 15:44:55 -0430

    first commit

---
+++ views/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Curriculum Vitae</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="../public/stylesheets/c3.css"> 
+    <link rel="stylesheet" href="../public/stylesheets/dark.css">
+</head>
+<body class="dark">
+   <div class="modo" id="modo">
+    <i class="fa fa-toggle-on" aria-hidden="true"></i>
+   </div> 
+    <div class="contenedor">
+        <div class="izquierdo">
+           
+           <div class="perfil">
+            <div class="img">
+                 <img src ="../public/stylesheets/PERFIL.jpg"  alt="esta imagen no se puede procesar">
+            </div>
+            <h2>Leidy Callejas</h2>
+        </div>
+        <div class="Datos">
+            <center><h3 class="title">Datos Personales</h3></center>
+            <ul>
+                <li>   
+                    <b>C.I.</b>30.276.772</p>
+                </li>
+                <li>
+                    <b>Fecha de nacimiento:</b> 14/10/2003
+                </li>
+                <li>
+                    <b>Lugar de Nacimiento:</b> San Juan de los Morros 
+                </li>
+                <li>
+                     <b>Direccion:</b> Urbanizacion la Tropical II 
+                </li>
+                <li>
+                    <b>Calle:</b> 11
+                </li>
+                <li>
+                    <b>Casa:</b>12<br>
+                </li>
+                
+            </ul>
+        </div><br>
+
+        <div class="info">
+            <h3 class="title">contacto</h3>
+            <ul>
+                <li>
+                    <span class="icon"><i class="fa fa-whatsapp" aria-hidden="true"></i></span>
+                    <span class="text">+58 426-192-7049</span>
+                </li>
+                <li>
+                    <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                    <span class="text">Lscallejas14@gmail.com</span>
+                </li>
+                <li>
+                    <span class="icon"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span>
+                    <a class="text" href="https://www.linkedin.com/in/leidy-callejas-76886b1b9/">www.linkedin.com/in/leidy-callejas-76886b1b9/</a>
+                </li>
+                <li>
+                    <span class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                    <span class="text">San Juan de los Morros,Guarico,Venezuela</span>
+                </li>
+                
+            </ul>
+        </div>
+        <div class="Educacion">
+            <h3 class="title">Informacion Academica</h3>
+            <ul>
+                <li>
+                  <h5>2015-2020 | San Juan de los Morros- Venezuela</h5> 
+                  <h4>Bachillerato</h4>
+                  <h4>U.E.C. Leon Topel Wortman</h4><br>
+                </li>
+                <li>
+                    <h5> 2020-En Curso | San Juan de los Morros- Venezuela</h5> 
+                    <h4>Ing. Informatica - 6to Semestre</h4>
+                    <h4>U.N.E "Romulo Gallegos"</h4>
+                  </li>
+                
+            </ul>
+        </div>
+    </div>
+
+  
+
+        <div class="derecho" >
+            <div class="Eventos">
+                <h2 class="title2">Eventos</h2>
+                <ul>
+                    <li>
+                      <h4>“Olimpiadas de Matematicas 2012”</h4> 
+                      <h3>Guárico, San Juan de lós Morros.</h3>
+                    
+                    </li>
+                    <li>
+                        <h4>“Olimpiadas de Matematicas 2013”.</h4> 
+                        <h3>Guárico, San Juan de lós Morros.</h3>
+                    </li>
+                    <li>
+                      <h4>“Olimpiadas de Matematicas 2014”</h4> 
+                      <h3>Guárico, San Juan de lós Morros.</h3>
+                    </li>
+                    <li>
+                      <h4>“Olimpiadas de Matematicas 2015”</h4> 
+                      <h3>Guárico, San Juan de lós Morros.</h3>
+                    </li>
+                
+                </ul>
+            </div> <hr>
+            
+            <div class="Redes">
+               <h2 class="title2">Redes Sociales</h2>
+               <ul>
+                <li>
+                    <span class="icon"><i class="fa fa-facebook-official" aria-hidden="true"></i></span>
+                    <a href="https://www.facebook.com/profile.php?id=100011427813500" class="text" target="_blank">Leidy Rodriguez</a>
+                </li>
+                <li>
+                    <span class="icon"><i class="fa fa-instagram" aria-hidden="true"></i></span>
+                    <a href="https://www.instagram.com/lsalo15/" class="text" target="_blank">Lsalo15</a>
+                </li>
+                <li>
+                    <span class="icon"><i class="fa fa-github" aria-hidden="true"></i></span>
+                   <a href="https://github.com/LsCallejas" class="text" target="_blank">LsCallejas</a> 
+                </li>
+          </div> <br><hr> 
+          <div class="skill">
+              <h2 class="title2">Skills</h2>
+              <div class="box">
+                  <h4>HTML</h4>
+                  <div class="percent">
+                      <div style="width: 80%;"></div>
+                  </div>
+              </div>
+              <div class="box">
+                <h4>CSS</h4>
+                <div class="percent">
+                    <div style="width: 80%;"></div>
+                </div>
+              </div>
+              <div class="box">
+                <h4>PHP</h4>
+                <div class="percent">
+                    <div style="width: 60%;"></div>
+                </div>
+              </div>  <br>
+ 
+          </div>
+          <div class="aficiones">
+            <h2 class="title2">aficiones</h2>
+            <ul>
+                <li><i class="fa fa-gamepad" aria-hidden="true"></i>Videojuegos</li>
+                <li><i class="fa fa-music" aria-hidden="true"></i>musica</li>
+                <li><i class="fa fa-trophy" aria-hidden="true"></i>artes marciales</li>
+                <li><i class="fa fa-puzzle-piece" aria-hidden="true"></i>puzzles</li>
+            </ul>
+           </div><hr>
+           <div class="cursos">
+               <h2 class="title2">Cursos</h2>
+               <ul>
+                   <li>
+                       <a href="https://www.coursera.org/account/accomplishments/certificate/4LKKZQAPTYTV" target="_blank"><h4>“Introduction to Psycology”</h4></a>
+                       <h3>Universidad de Yale.(1 mes)</h3>
+                   </li><br>
+                   <li> 
+                       <a href="https://www.futurelearn.com/certificates/k47jd0y" target="_blank"><h4>“Basic English 1: Elementary”</h4></a>
+                       <h3>King’s College London (16h)</h3>
+                   </li>
+                   <li> <br>
+                    <a href="https://www.coursera.org/account/accomplishments/certificate/2JE3WVCXEGHA" target="_blank"><h4>“Cybersecurity for Managers”</h4></a>
+                    <h3>Campus BBVA (8h)</h3>
+                </li>
+               </ul>
+           </div>
+        </div>
+    </div>
+    <script src="../public/js/main.js"></script>
+
+
+<tbody class="body2 dark2">
+
+        <div class="contact">
+            <h1 class="logo">Contact <span>Us</span></h1>
+            
+            <form action="" method="post">
+                <?php require '../includes/email.php'?>
+               
+                    <input type="text" name="nombre" placeholder="nombre" required>
+                    <input type="email" name="email" placeholder="email" required>
+                    <textarea name="Comentario"  rows="10" placeholder="Escribir mensaje" required></textarea>
+                     <input type="submit" value="ENVIAR" id="boton">
+                     
+            </form>
+            
+            
+        </div> 
+</tbody>
+</body>
+</html>
